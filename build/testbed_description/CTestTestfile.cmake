4269ce6812af85885ae01812a757e31724cc5d33
Deep2k24 <deepsonawane.robotics@gmail.com>
2026-04-23 01:09:54 +0530

    Delete build directory

--- build/testbed_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/deep/assignment_ws/src/ros_nav2_assignment/testbed_description
-# Build directory: /home/deep/assignment_ws/build/testbed_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/testbed_gazebo/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/deep/assignment_ws/install/testbed_description/share/testbed_description/package.sh"
